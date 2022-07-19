9179772ec41517f292606619acd6e4277b1100c5
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-07-19 13:00:39 +0000

    Apply auto-formatting rules

--- infrastructure/terraform.tf
+++ infrastructure/terraform.tf
@@ -2,15 +2,15 @@ terraform {
   backend "s3" {
     role_arn = "arn:aws:iam::756629837203:role/catalogue-developer"
 
-    bucket = "wellcomecollection-catalogue-infra-delta"
-    key = "terraform/concepts-pipeline/concepts_pipeline.tfstate"
+    bucket         = "wellcomecollection-catalogue-infra-delta"
+    key            = "terraform/concepts-pipeline/concepts_pipeline.tfstate"
     dynamodb_table = "terraform-locktable"
-    region = "eu-west-1"
+    region         = "eu-west-1"
   }
 
   required_providers {
     aws = {
-      source = "hashicorp/aws"
+      source  = "hashicorp/aws"
       version = "~> 4.22.0"
     }
   }
