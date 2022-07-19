e219913bd1a8d88a6c755b933242955b8c6b4087
jamie <j.parkinson@wellcome.ac.uk>
2022-07-19 13:15:04 +0100

    Terraform fmt in CI too

--- .buildkite/scripts/autoformat.sh
+++ .buildkite/scripts/autoformat.sh
@@ -2,18 +2,25 @@
 set -euo pipefail
 
 SCALAFMT_VERSION=3.5.8
+TERRAFORM_VERSION=1.2.5
 ROOT=$(git rev-parse --show-toplevel)
 PATH=$HOME/.local/bin:$PATH
 
-# Install the formatter if it's not already present on the agent
+# Install the formatters if they're not already present on the agent
 if [[ ! -x "$(command -v scalafmt-native)" || "$(scalafmt-native --version)" != "$SCALAFMT_VERSION" ]]; then
   mkdir -p $HOME/.local/bin
   curl https://raw.githubusercontent.com/scalameta/scalafmt/master/bin/install-scalafmt-native.sh | \
     bash -s -- $SCALAFMT_VERSION $HOME/.local/bin/scalafmt-native
 fi
+if [[ ! -x "$(command -v terraform)" || "$(terraform --version)" != "$TERRAFORM_VERSION" ]]; then
+  mkdir -p $HOME/.local/bin
+  wget -O /tmp/terraform.zip https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+  unzip -q -d $HOME/.local/bin /tmp/terraform.zip
+fi
 
-# Run the formatter
+# Run the formatters
 scalafmt-native --non-interactive $ROOT
+terraform fmt -recursive $ROOT
 
 # Commit any changes
 if [[ `git status --porcelain` ]]; then
