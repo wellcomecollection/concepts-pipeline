9fe19a07990da101e5f876d19f8d045c7ce54f17
jamie <j.parkinson@wellcome.ac.uk>
2022-07-19 12:59:15 +0100

    Need to print the baseDirectory

--- .buildkite/scripts/run_sbt_job.sh
+++ .buildkite/scripts/run_sbt_job.sh
@@ -11,7 +11,7 @@ CATALOGUE_ECR_REPO="756629837203.dkr.ecr.eu-west-1.amazonaws.com"
 $SBT "project $PROJECT" "test"
 
 if [ "$BUILDKITE_BRANCH" == "ingestor-ecr" ]; then
-  PROJECT_DIR=$($SBT -error "project $PROJECT" "stage; baseDirectory")
+  PROJECT_DIR=$($SBT -error "project $PROJECT" "stage; print baseDirectory")
   IMAGE_TAG="$PROJECT:ref.$BUILDKITE_COMMIT"
 
   docker build "$ROOT/$PROJECT_DIR" --tag $IMAGE_TAG
