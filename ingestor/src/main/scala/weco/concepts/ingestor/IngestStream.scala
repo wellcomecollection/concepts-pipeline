69175bc29b1293f612b1457649b878189d4a1124
jamie <j.parkinson@wellcome.ac.uk>
2022-08-01 17:39:52 +0100

    Add a sink that tells us what happened

--- ingestor/src/main/scala/weco/concepts/ingestor/IngestStream.scala
+++ ingestor/src/main/scala/weco/concepts/ingestor/IngestStream.scala
@@ -3,17 +3,32 @@ package weco.concepts.ingestor
 import akka.Done
 import akka.actor.ActorSystem
 import akka.stream.scaladsl.Sink
+import akka.util.ByteString
 import grizzled.slf4j.Logging
 import weco.concepts.ingestor.stages.Fetcher
 
-import scala.concurrent.Future
+import scala.concurrent.{ExecutionContext, Future}
 
 class IngestStream(dataUrl: String)(implicit actorSystem: ActorSystem)
     extends Logging {
+  implicit val executionContext: ExecutionContext = actorSystem.dispatcher
+
   lazy val fetcher = new Fetcher()
 
   def run: Future[Done] =
     fetcher
       .fetchFromUrl(dataUrl)
-      .runWith(Sink.ignore)
+      .runWith(
+        // Count the number of bytes we've seen
+        // This is just while future stages don't exist
+        Sink.fold[(Int, Long), ByteString]((0, 0L)) {
+          case ((nChunks, total), byteString) =>
+            val length = byteString.length
+            (nChunks + 1, total + length)
+        }
+      )
+      .map { case (nChunks, totalBytes) =>
+        info(s"Streamed $totalBytes bytes in $nChunks chunks")
+        Done
+      }
 }
