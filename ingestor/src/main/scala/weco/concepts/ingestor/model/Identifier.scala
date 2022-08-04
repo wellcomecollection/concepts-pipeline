75a7d858c4196fe8a7734fcc03e942c13dc348b0
jamie <j.parkinson@wellcome.ac.uk>
2022-08-04 16:16:57 +0100

    Add a model for indexable concept

---
+++ ingestor/src/main/scala/weco/concepts/ingestor/model/Identifier.scala
@@ -0,0 +1,18 @@
+// TODO: These will probably end up living in a common project/library in this repo
+package weco.concepts.ingestor.model
+
+case class Identifier(
+  value: String,
+  identifierType: IdentifierType,
+  ontologyType: "Identifier" = "Identifier"
+)
+
+sealed trait IdentifierType {
+  val id: String;
+  override def toString: String = id
+}
+object IdentifierType {
+  case object LCSubjects extends IdentifierType {
+    val id = "lc-subjects"
+  }
+}
