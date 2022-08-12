fdf95ec02a3a7d81477de4067ac3c69c10908220
Paul Butcher <paul.butcher@gmail.com>
2022-08-12 11:56:33 +0100

    make tail recursive

--- common/src/main/scala/weco/concepts/common/model/UsedConcept.scala
+++ common/src/main/scala/weco/concepts/common/model/UsedConcept.scala
@@ -1,5 +1,9 @@
 package weco.concepts.common.model
 
+/**
+ * A UsedConcept represents a concept in use in Works in the Catalogue
+ * It links a Wellcome canonicalId to an identifier from an external authority
+ */
 case class UsedConcept(
   identifier: Identifier,
   label: String,
