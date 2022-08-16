2f568faed0d457326a0256ebb8034d92362920f5
Paul Butcher <paul.butcher@gmail.com>
2022-08-16 14:04:34 +0100

    alphabetize id types

--- common/src/main/scala/weco/concepts/common/model/Identifier.scala
+++ common/src/main/scala/weco/concepts/common/model/Identifier.scala
@@ -12,17 +12,17 @@ sealed trait IdentifierType {
 }
 
 object IdentifierType {
-  case object LCSubjects extends IdentifierType {
-    val id = "lc-subjects"
+  case object LabelDerived extends IdentifierType {
+    val id = "label-derived"
   }
   case object LCNames extends IdentifierType {
     val id = "lc-names"
   }
+  case object LCSubjects extends IdentifierType {
+    val id = "lc-subjects"
+  }
   case object MeSH extends IdentifierType {
     val id = "nlm-mesh"
-  }
-  case object LabelDerived extends IdentifierType {
-    val id = "label-derived"
   }
   val typeMap: Map[String, IdentifierType] =
     Seq(LCSubjects, LCNames, MeSH, LabelDerived).map(i => i.id -> i).toMap
