22a74626f25d46ead7457fd997486f7be29a60d2
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-08-17 08:22:24 +0000

    Apply auto-formatting rules

--- aggregator/src/main/scala/weco/concepts/aggregator/AggregateStream.scala
+++ aggregator/src/main/scala/weco/concepts/aggregator/AggregateStream.scala
@@ -10,7 +10,8 @@ import weco.concepts.common.source.{Fetcher, Scroll}
 
 import scala.concurrent.{ExecutionContext, Future}
 
-class AggregateStream(dataUrl: String) (implicit actorSystem: ActorSystem) extends Logging {
+class AggregateStream(dataUrl: String)(implicit actorSystem: ActorSystem)
+    extends Logging {
   implicit val executionContext: ExecutionContext = actorSystem.dispatcher
 
   lazy val fetcher = new Fetcher(Http().superPool())
@@ -18,26 +19,26 @@ class AggregateStream(dataUrl: String) (implicit actorSystem: ActorSystem) exten
     // TODO: Move this bit to its own function, somewhere else
     // That will allow me to inject it so that I can run this stream
     // over STDIN.
-  fetcher
-    .fetchFromUrl(dataUrl)
-    // At time of writing, the largest JSON in the catalogue is ca 475KiB
-    // 0.5 MiB should give sufficient overhead to allow for expansion in the
-    // longest catalogue entries.
-    // When running over the whole catalogue like this, it will be essentially under
-    // human control, so the number can be tweaked if required, rather than causing a
-    // failure in an unsupervised system.
-    .via(Scroll(512 * 1024))
-    // TODO END
-    .via(extractConceptsFlow)
+    fetcher
+      .fetchFromUrl(dataUrl)
+      // At time of writing, the largest JSON in the catalogue is ca 475KiB
+      // 0.5 MiB should give sufficient overhead to allow for expansion in the
+      // longest catalogue entries.
+      // When running over the whole catalogue like this, it will be essentially under
+      // human control, so the number can be tweaked if required, rather than causing a
+      // failure in an unsupervised system.
+      .via(Scroll(512 * 1024))
+      // TODO END
+      .via(extractConceptsFlow)
 //    .via(saveConceptsFlow)
-    // TODO: Also report on the number of concepts
-    .runWith(
-      Sink.fold(0L)((nLines, _) => nLines + 1)
-    )
-    .map(nLines => {
-      info(s"Transformed $nLines lines from $dataUrl")
-      Done
-    })
+      // TODO: Also report on the number of concepts
+      .runWith(
+        Sink.fold(0L)((nLines, _) => nLines + 1)
+      )
+      .map(nLines => {
+        info(s"Transformed $nLines lines from $dataUrl")
+        Done
+      })
   }
 
   def extractConceptsFlow: Flow[String, Seq[UsedConcept], NotUsed] =
