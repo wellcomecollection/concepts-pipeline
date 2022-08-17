bf5f4ada9ce1d3667dd60678e9adb0949aa90941
Paul Butcher <paul.butcher@gmail.com>
2022-08-17 13:19:34 +0100

    improve commentary

--- aggregator/src/main/scala/weco/concepts/aggregator/sources/StdInSource.scala
+++ aggregator/src/main/scala/weco/concepts/aggregator/sources/StdInSource.scala
@@ -6,8 +6,10 @@ import akka.util.ByteString
 import weco.concepts.common.source.Scroll
 
 object StdInSource {
-  def apply: Source[String, NotUsed] = {
+  def apply(): Source[String, NotUsed] = {
     Source(
+      // Produce a single ByteString, in the same fashion
+      // as the Fetcher
       Seq(ByteString(System.in.readAllBytes()))
     ).via(Scroll.fromUncompressed(512 * 1024))
   }
