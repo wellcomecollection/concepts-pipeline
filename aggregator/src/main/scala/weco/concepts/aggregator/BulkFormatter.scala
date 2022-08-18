d3e5f66c5383f4a5db030aadc265c2e97b3f99b4
Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2022-08-18 14:45:36 +0000

    Apply auto-formatting rules

--- aggregator/src/main/scala/weco/concepts/aggregator/BulkFormatter.scala
+++ aggregator/src/main/scala/weco/concepts/aggregator/BulkFormatter.scala
@@ -2,13 +2,17 @@ package weco.concepts.aggregator
 
 import weco.concepts.common.model.UsedConcept
 
-/**
- * Formatter to turn concepts into couplets for use in an Elasticsearch Bulk
- * request.
- */
+/** Formatter to turn concepts into couplets for use in an Elasticsearch Bulk
+  * request.
+  */
 class BulkFormatter(index: String) {
   def format(concept: UsedConcept): String = {
-    val action = ujson.Obj("create" -> ujson.Obj("_index" -> index, "_id" -> concept.identifier.toString))
+    val action = ujson.Obj(
+      "create" -> ujson.Obj(
+        "_index" -> index,
+        "_id" -> concept.identifier.toString
+      )
+    )
     val document = ujson.Obj(
       "identifier" -> concept.identifier.toString,
       "label" -> concept.label,
