5566fa47871e0917d9bdb2d964f12a727071774b
Paul Butcher <paul.butcher@gmail.com>
2022-09-26 13:11:34 +0100

    Terraform the Aggregator Lambda (#53)
    * Start trying a bit of lambda
    
    * more lambda experiments
    
    * notes
    
    * working on lambda
    
    * Add and configure EC provider
    
    * Add a concepts cluster which uses the catalogue VPCE
    
    * Provision users in Elasticsearch cluster
    
    * Password hash was wrong
    
    * Try to sort out roles a bit
    
    * Fix names
    
    * HEADing an index requires permissions to view metadata
    
    * Store public/private hosts in secrets
    
    * Add a script for connecting to the remote cluster
    
    * tidy whitespace
    
    * a whole bunch of lambda-based changes
    
    * tidy logging
    
    * improve commentary
    
    * final attempt at lambda, it is not working out
    
    * It actually works!  Lambda is just very slow to connect to Secrets
    
    * Apply auto-formatting rules
    
    * separate fetching of AWS secrets
    
    * Apply auto-formatting rules
    
    * Move Indexer to common
    
    * Move BulkUpdateFlow to common
    
    * Use a typeclass to format stuff
    
    * That was a pointless use of implicits
    
    * Use the indexer and bulk updater in the ingestor
    
    * Moving stuff around to run locally
    
    * Add TCP keepalive
    
    * Be less clever with SAMs
    
    * Add a README
    
    * run full update on Lambda
    
    * update gjson
    
    * async does not make a significant difference
    
    * update lambda doc to include extra time fetching credentials
    
    * update docs
    
    * first cut with role permissions placeholder
    
    * move into stack
    
    * move into stack
    
    * Terraform the Aggregator Lambda
    
    * Apply auto-formatting rules
    
    * tidy whitespace
    
    Co-authored-by: jamie <j.parkinson@wellcome.ac.uk>
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

---
+++ infrastructure/stack/aggregator_lambda.tf
@@ -0,0 +1,96 @@
+#
+# The Concepts Aggregator Lambda function
+#
+# This manages the lambda function for the concepts aggregator.
+# The function fetches Works from the Catalogue API and stores any
+# used concepts in Elasticsearch.
+#
+locals {
+  ecr_image_tag = "latest" # for now.  Once we make it work like weco-deploy, use the namespace to tag it.
+  service_name  = "concepts_aggregator"
+  secrets = {
+    elasticsearch_host = {
+      name = "elasticsearch/concepts-${var.namespace}/public_host"
+      arn  = module.host_secrets.arns[1]
+    }
+    elasticsearch_password = {
+      name = module.client_service_users["aggregator"].password_secret_name
+      arn  = module.client_service_users["aggregator"].password_secret_arn
+    }
+  }
+}
+
+data "aws_ecr_image" "lambda_image" {
+  repository_name = var.aggregator_repository.name
+  image_tag       = local.ecr_image_tag
+}
+
+
+data "aws_iam_policy_document" "concepts_aggregator_permissions" {
+  statement {
+    actions = [
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    effect    = "Allow"
+    resources = ["*"]
+    sid       = "CreateCloudWatchLogs"
+  }
+  statement {
+    actions = [
+      "secretsmanager:GetSecretValue"
+    ]
+    effect = "Allow"
+    resources = [
+      local.secrets.elasticsearch_host.arn,
+      local.secrets.elasticsearch_password.arn
+    ]
+  }
+}
+
+
+resource "aws_iam_role" "concepts_aggregator_role" {
+  name               = "${var.namespace}-concepts_aggregator_role"
+  assume_role_policy = <<EOF
+{
+   "Version": "2012-10-17",
+   "Statement": [
+       {
+           "Action": "sts:AssumeRole",
+           "Principal": {
+               "Service": "lambda.amazonaws.com"
+           },
+           "Effect": "Allow"
+       }
+   ]
+}
+ EOF
+}
+
+
+resource "aws_iam_policy" "concepts_aggregator_policy" {
+  name   = "${var.namespace}-lambda-policy"
+  policy = data.aws_iam_policy_document.concepts_aggregator_permissions.json
+}
+
+resource "aws_iam_role_policy_attachment" "concepts_aggregator_attachment" {
+  role       = aws_iam_role.concepts_aggregator_role.name
+  policy_arn = aws_iam_policy.concepts_aggregator_policy.arn
+}
+
+resource "aws_lambda_function" "concepts_aggregator" {
+  function_name = "${var.namespace}-concepts_aggregator"
+  package_type  = "Image"
+  image_uri     = "${var.aggregator_repository.url}@${data.aws_ecr_image.lambda_image.id}"
+  timeout       = 90
+  memory_size   = 1024
+  environment {
+    variables = {
+      AGGREGATOR_APP_CONTEXT = "remote"
+      es_host                = local.secrets.elasticsearch_host.name
+      es_password            = local.secrets.elasticsearch_password.name
+    }
+  }
+  role = aws_iam_role.concepts_aggregator_role.arn
+}
