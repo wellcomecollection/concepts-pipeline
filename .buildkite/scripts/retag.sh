e81da94ea70ce5bdf5a7d8e800601b0b081ed229
Paul Butcher <paul.butcher@gmail.com>
2022-09-28 16:26:19 +0100

    use correct ref tag

--- .buildkite/scripts/retag.sh
+++ .buildkite/scripts/retag.sh
@@ -1,5 +1,5 @@
 AGGREGATOR_IMAGE_TAG=$1
 PIPELINE_NAMESPACE=$2
 
-MANIFEST=$(aws ecr batch-get-image --repository-name weco/concepts_aggregator --image-ids imageTag=AGGREGATOR_IMAGE_TAG --output json | jq --raw-output --join-output '.images[0].imageManifest')
+MANIFEST=$(aws ecr batch-get-image --repository-name weco/concepts_aggregator --image-ids imageTag=ref.${AGGREGATOR_IMAGE_TAG} --output json | jq --raw-output --join-output '.images[0].imageManifest')
 aws ecr put-image --repository-name weco/concepts_aggregator  --image-tag ${PIPELINE_NAMESPACE} --image-manifest "$MANIFEST"
