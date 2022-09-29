b40eda6dc076322628bf00fdcb3e5c36980aabf1
Paul Butcher <paul.butcher@gmail.com>
2022-09-29 13:30:56 +0100

    just push the tag

--- .buildkite/scripts/deploy_to_live.sh
+++ .buildkite/scripts/deploy_to_live.sh
@@ -8,5 +8,5 @@ echo "current lambda configuration, before change"
 aws lambda get-function-configuration --function-name ${PIPELINE_NAMESPACE}-concepts_aggregator
 
 echo "updating lambda configuration"
-sh $HERE/retag.sh weco/concepts_aggregator ref.${AGGREGATOR_IMAGE_TAG} $PIPELINE_NAMESPACE
+#sh $HERE/retag.sh weco/concepts_aggregator ref.${AGGREGATOR_IMAGE_TAG} $PIPELINE_NAMESPACE
 sh $HERE/notify_services.sh $PIPELINE_NAMESPACE
