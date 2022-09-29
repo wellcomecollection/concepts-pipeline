bd67afa45bacaac3c18b4ffa9fedf26016543d86
Paul Butcher <paul.butcher@gmail.com>
2022-09-29 11:36:37 +0100

    make sure image exists before using it

--- .buildkite/scripts/retag.sh
+++ .buildkite/scripts/retag.sh
@@ -4,4 +4,7 @@ EXISTING_TAG=$2
 NEW_TAG=$3
 
 MANIFEST=$(aws ecr batch-get-image --repository-name $REPOSITORY --image-ids imageTag=${EXISTING_TAG} --output json | jq --raw-output --join-output '.images[0].imageManifest')
+
+echo $MANIFEST
+
 aws ecr put-image --repository-name $REPOSITORY  --image-tag ${NEW_TAG} --image-manifest "$MANIFEST"
