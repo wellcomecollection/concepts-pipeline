514c4eefdd591034167880b3c1381b5897055791
Paul Butcher <paul.butcher@gmail.com>
2022-10-03 15:25:28 +0100

    Make ingestore work under Lambda (RIE, locally)

--- ingestor/src/main/scala/weco/concepts/ingestor/IngestorMain.scala
+++ ingestor/src/main/scala/weco/concepts/ingestor/IngestorMain.scala
@@ -1,24 +1,30 @@
 package weco.concepts.ingestor
 
 import akka.actor.ActorSystem
-import com.typesafe.config.ConfigFactory
+import com.typesafe.config.{Config, ConfigFactory}
+import grizzled.slf4j.Logging
 import net.ceedubs.ficus.Ficus._
 import net.ceedubs.ficus.readers.ArbitraryTypeReader._
 import weco.concepts.common.elasticsearch.ElasticAkkaHttpClient
 
 import scala.concurrent.ExecutionContext
 
-trait IngestorMain {
-  val config = ConfigFactory.load()
+trait IngestorMain extends Logging {
+  private val config: Config = {
+    val configName = sys.env.getOrElse("INGESTOR_APP_CONTEXT", "local")
+    info(s"loading config $configName")
+    ConfigFactory.load(configName)
+  }
   val lcshUrl = config.as[String]("data-source.loc.lcsh")
   val lcNamesUrl = config.as[String]("data-source.loc.names")
   val indexName = config.as[String]("data-target.index.name")
   val maxBulkRecords = config.as[Int]("data-target.bulk.max-records")
-  val clusterConfig =
-    config.as[ElasticAkkaHttpClient.ClusterConfig]("data-target.cluster")
 
   implicit val actorSystem: ActorSystem = ActorSystem("main")
   implicit val executionContext: ExecutionContext = actorSystem.dispatcher
+
+  val clusterConfig =
+    config.as[ElasticAkkaHttpClient.ClusterConfig]("data-target.cluster")
 
   val elasticHttpClient = ElasticAkkaHttpClient(clusterConfig)
   val ingestStream = new IngestStream(
