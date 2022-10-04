831ea26e2f908d30869ebf73616e755787bdd842
Paul Butcher <paul.butcher@gmail.com>
2022-10-04 15:25:43 +0100

    concept -> concepts

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -4,5 +4,5 @@ ROOT=$(git rev-parse --show-toplevel)
 
 echo "deploying $BUILD_REF to $PIPELINE_NAMESPACE"
 
-sh "$ROOT/scripts/notify_lambda.sh" "$PIPELINE_NAMESPACE" "concept_aggregator"
-sh "$ROOT/scripts/notify_lambda.sh" "$PIPELINE_NAMESPACE" "concept_ingestor"
+sh "$ROOT/scripts/notify_lambda.sh" "$PIPELINE_NAMESPACE" "concepts_aggregator"
+sh "$ROOT/scripts/notify_lambda.sh" "$PIPELINE_NAMESPACE" "concepts_ingestor"
