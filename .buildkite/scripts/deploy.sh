b8e4a50e4db41075f025d606e387601d23b0c503
Paul Butcher <paul.butcher@gmail.com>
2022-10-04 15:09:29 +0100

    Deploy ingestor on CO

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -4,4 +4,5 @@ ROOT=$(git rev-parse --show-toplevel)
 
 echo "deploying $BUILD_REF to $PIPELINE_NAMESPACE"
 
-sh "$ROOT/scripts/notify_services.sh" "$PIPELINE_NAMESPACE"
+sh "$ROOT/scripts/notify_lambda.sh" "$PIPELINE_NAMESPACE" "concept_aggregator"
+sh "$ROOT/scripts/notify_lambda.sh" "$PIPELINE_NAMESPACE" "concept_ingestor"
--- scripts/notify_lambda.sh
+++ scripts/notify_lambda.sh
@@ -1,9 +1,10 @@
 # Tell the lambda to update its image
 PIPELINE_NAMESPACE=$1
+SERVICE_NAME=$2
 
 # Print out what is currently there.
 echo "current lambda configuration, before change"
-aws lambda get-function-configuration --function-name ${PIPELINE_NAMESPACE}-concepts_aggregator
+aws lambda get-function-configuration --function-name ${PIPELINE_NAMESPACE}-${SERVICE_NAME}
 
 # It may seem counterintuitive,
 # that this call is always the same on a pipeline
@@ -13,10 +14,10 @@ aws lambda get-function-configuration --function-name ${PIPELINE_NAMESPACE}-conc
 
 echo "updating lambda configuration"
 # https://docs.aws.amazon.com/lambda/latest/dg/python-image.html#python-image-deploy
-aws lambda update-function-code --function-name ${PIPELINE_NAMESPACE}-concepts_aggregator --image-uri 756629837203.dkr.ecr.eu-west-1.amazonaws.com/weco/concepts_aggregator:${PIPELINE_NAMESPACE}
+aws lambda update-function-code --function-name ${PIPELINE_NAMESPACE}-${SERVICE_NAME} --image-uri 756629837203.dkr.ecr.eu-west-1.amazonaws.com/weco/${SERVICE_NAME}:${PIPELINE_NAMESPACE}
 
 # Waiting here and then reading the configuration is not a necessary step,
 # but it may aid investigations if something goes awry.
-aws lambda wait function-updated --function-name ${PIPELINE_NAMESPACE}-concepts_aggregator
-aws lambda get-function-configuration --function-name ${PIPELINE_NAMESPACE}-concepts_aggregator
+aws lambda wait function-updated --function-name ${PIPELINE_NAMESPACE}-${SERVICE_NAME}
+aws lambda get-function-configuration --function-name ${PIPELINE_NAMESPACE}-${SERVICE_NAME}
 
