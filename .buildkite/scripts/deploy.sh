6b789af2cc8894cb2fb54a10efc349888e43965f
Paul Butcher <paul.butcher@gmail.com>
2022-10-10 11:24:55 +0100

    Deploy ingestor via CI (#60)
    * ensure actorsystem is initalised before being used
    
    * build and use ingestor uberjar
    
    * run ingestor in Lambda
    
    * Make ingestore work under Lambda (RIE, locally)
    
    * terraform for ingestor lambda
    
    * Apply auto-formatting rules
    
    * allow ingestor to get secrets when running in Lambda
    
    * disambiguate io.Source
    
    * All working on Lambda
    
    * Apply auto-formatting rules
    
    * Deploy ingestor on CO
    
    * resolveSecrets is now required
    
    * concept -> concepts
    
    * clarify output
    
    * deploy only on main branch
    
    * drive deploy of each service from buildkite def
    
    Co-authored-by: Buildkite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>

--- .buildkite/scripts/deploy.sh
+++ .buildkite/scripts/deploy.sh
@@ -1,7 +1,8 @@
 PIPELINE_NAMESPACE=$1
-BUILD_REF=$2
+SERVICE=$2
+BUILD_REF=$3
 ROOT=$(git rev-parse --show-toplevel)
 
 echo "deploying $BUILD_REF to $PIPELINE_NAMESPACE"
 
-sh "$ROOT/scripts/notify_services.sh" "$PIPELINE_NAMESPACE"
+sh "$ROOT/scripts/notify_lambda.sh" "$PIPELINE_NAMESPACE" $SERVICE
