85473b44ca3b45fcd41c75187f9c12f5fe5aa058
jamie <j.parkinson@wellcome.ac.uk>
2022-10-27 17:48:11 +0100

    Implement SQSMain

---
+++ recorder/src/main/scala/weco/concepts/recorder/RecorderMain.scala
@@ -0,0 +1,40 @@
+package weco.concepts.recorder
+
+import akka.actor.ActorSystem
+import com.typesafe.config.{Config, ConfigFactory}
+import net.ceedubs.ficus.Ficus._
+import net.ceedubs.ficus.readers.ArbitraryTypeReader._
+import grizzled.slf4j.Logging
+import weco.concepts.common.elasticsearch.{
+  ElasticAkkaHttpClient,
+  ElasticHttpClient
+}
+import weco.concepts.common.secrets.{ClusterConfWithSecrets, SecretsResolver}
+
+import scala.concurrent.ExecutionContext
+
+trait RecorderMain extends Logging {
+  private val config: Config = {
+    val configName = sys.env.getOrElse("APP_CONTEXT", "local")
+    info(s"loading config $configName")
+    ConfigFactory.load(configName)
+  }
+  implicit val actorSystem: ActorSystem = ActorSystem("main")
+  implicit val executionContext: ExecutionContext = actorSystem.dispatcher
+
+  private val clusterConfig = new ClusterConfWithSecrets(
+    SecretsResolver(config.as[String]("secrets-resolver"))
+  )(config.as[ElasticAkkaHttpClient.ClusterConfig]("data.cluster"))
+
+  private val elasticHttpClient: ElasticHttpClient = ElasticAkkaHttpClient(
+    clusterConfig
+  )
+
+  val recorderStream: RecorderStream = new RecorderStream(
+    elasticHttpClient = elasticHttpClient,
+    authoritativeConceptsIndexName =
+      config.as[String]("data-source.index.authoritative.name"),
+    usedConceptsIndexName = config.as[String]("data-source.index.used.name"),
+    targetIndexName = config.as[String]("data-target.index.name")
+  )
+}
--- recorder/src/main/scala/weco/concepts/recorder/SQSMain.scala
+++ recorder/src/main/scala/weco/concepts/recorder/SQSMain.scala
@@ -1,10 +1,15 @@
 package weco.concepts.recorder
 
+import akka.Done
+import akka.stream.scaladsl.{Sink, Source}
 import com.amazonaws.services.lambda.runtime.{Context, RequestHandler}
 import grizzled.slf4j.Logging
 import com.amazonaws.services.lambda.runtime.events.SQSEvent
 import com.amazonaws.services.lambda.runtime.events.SQSEvent.SQSMessage
+import weco.concepts.common.elasticsearch.BulkUpdateResult
 
+import scala.concurrent.Await
+import scala.concurrent.duration.DurationInt
 import scala.jdk.CollectionConverters._
 
 /*
@@ -17,7 +22,10 @@ import scala.jdk.CollectionConverters._
  * See: https://docs.aws.amazon.com/lambda/latest/dg/with-sqs.html
  */
 
-object SQSMain extends RequestHandler[SQSEvent, String] with Logging {
+object SQSMain
+    extends RequestHandler[SQSEvent, String]
+    with Logging
+    with RecorderMain {
 
   override def handleRequest(
     event: SQSEvent,
@@ -27,9 +35,31 @@ object SQSMain extends RequestHandler[SQSEvent, String] with Logging {
     val conceptIds = recordList.map(_.getBody)
 
     info(
-      s"running recorder lambda over ${conceptIds.length} concepts: $conceptIds, Lambda request: ${context.getAwsRequestId}"
+      s"Running recorder lambda over ${conceptIds.length} concepts: $conceptIds, Lambda request: ${context.getAwsRequestId}"
     )
+
+    val f = Source(conceptIds)
+      .via(recorderStream.recordIds)
+      .runWith(Sink.fold[Long, BulkUpdateResult](0L)(_ + _.updated.size))
+      .map { total =>
+        info(s"Recorded $total concepts")
+        Done
+      }
+
+    // Wait here so that lambda can run correctly.
+    // Without waiting here, handleRequest finishes immediately.
+    // Locally, (in a lambda container), that results in
+    // the Lambda Runtime Interface Emulator telling us it took no time at all
+    // and then Akka starts doing all the work.
+    // I don't know what will happen in real life, but I suspect
+    // that Lambda will shutdown the container and nothing will get done.
+    Await.result(f, 10 minutes)
     "Done"
+    // Do not clean up or shutdown resources.
+    // If the same container is invoked again, then the actorSystem and db connection
+    // can be reused.  This speeds up the processing of subsequent calls
+    // When the Lambda expires (in 5 minutes or so), the whole container
+    // will be destroyed, so everything will be cleaned up anyway.
   }
 
 }
