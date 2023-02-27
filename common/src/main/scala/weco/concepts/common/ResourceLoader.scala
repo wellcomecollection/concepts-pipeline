9b199a54dfdb90c977d84c1740444da738641f1d
Paul Butcher <paul.butcher@gmail.com>
2023-02-27 13:40:31 +0000

    improve readability of append fields script

--- common/src/main/scala/weco/concepts/common/ResourceLoader.scala
+++ common/src/main/scala/weco/concepts/common/ResourceLoader.scala
@@ -7,7 +7,17 @@ trait ResourceLoader {
 
 object ResourceLoader extends ResourceLoader {
 
+  /*
+   * Load the named resource to a string.
+   *
+   * This method deliberately replaces any newlines in the source with a single space.
+   * In doing so, it allows the source JSON file to contain line breaks for clarity
+   * while still resulting in valid JSON when loaded.
+   *
+   * This is particularly useful for Elasticsearch Scripts, which are somewhat
+   * unreadable when squished on to a single line.
+   */
   def loadJsonResource(name: String): String =
-    Source.fromResource(s"$name.json").getLines().mkString(" ")
+    Source.fromResource(s"$name.json").getLines().mkString("")
 
 }
