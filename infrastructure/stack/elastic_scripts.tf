617d38567ee907d111e7156c724e0bca14da8d2b
Paul Butcher <paul.butcher@gmail.com>
2023-03-15 16:02:57 +0000

    tidy up

--- infrastructure/stack/elastic_scripts.tf
+++ infrastructure/stack/elastic_scripts.tf
@@ -1,5 +1,5 @@
 // A script used during an update to append new values to selected fields.
-//
+
 resource "elasticstack_elasticsearch_script" "append_fields" {
   script_id = "append-fields"
   lang      = "painless"
@@ -18,6 +18,6 @@ resource "elasticstack_elasticsearch_script" "append_fields" {
   // Store the script in the update context.
   // For some reason, if you store it without context, it will
   // recompile on each use, and then fail because you are running too many
-  // script compilations during a bulk update.d
+  // script compilations during a bulk update.
   context = "update"
 }
