dc6971f08ea4919f7b2d87a975766d5853f8abfa
Github on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-10-28 15:09:37 +0000

    Apply auto-formatting rules

--- infrastructure/2022-08-31/main.tf
+++ infrastructure/2022-08-31/main.tf
@@ -10,9 +10,9 @@ module "concepts_pipeline" {
   network_config     = local.network_config
   logging_cluster_id = local.logging_cluster_id
 
-  aggregator_repository = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_aggregator
+  aggregator_repository      = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_aggregator
   aggregator_bulk_repository = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_aggregator_bulk
-  ingestor_repository = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_ingestor
-  recorder_repository = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_recorder
-  recorder_bulk_repository = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_recorder_bulk
+  ingestor_repository        = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_ingestor
+  recorder_repository        = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_recorder
+  recorder_bulk_repository   = data.terraform_remote_state.concepts_shared.outputs.ecr_repository_concepts_recorder_bulk
 }
