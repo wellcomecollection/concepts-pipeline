396934fc32806ecff5414a07581d7a81b00867ca
Štěpán Brychta <stepan.brychta@me.com>
2024-11-05 12:01:08 +0000

    Refactor ingestor Main and run_with_remote_cluster.sh #5815

--- ingestor/run_with_remote_cluster.sh
+++ ingestor/run_with_remote_cluster.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-AWS_CLI_PROFILE="concepts-aggregator-local"
-CATALOGUE_DEVELOPER_ARN="arn:aws:iam::756629837203:role/catalogue-developer"
-ROOT=$(git rev-parse --show-toplevel)
-
 CONCEPTS_PIPELINE_DATE=$1
 if [ "$CONCEPTS_PIPELINE_DATE" = "" ]; then
     echo "You must provide a pipeline date!"
@@ -12,28 +8,16 @@ if [ "$CONCEPTS_PIPELINE_DATE" = "" ]; then
 fi
 shift
 
-aws configure set region eu-west-1 --profile $AWS_CLI_PROFILE
-aws configure set role_arn $CATALOGUE_DEVELOPER_ARN --profile $AWS_CLI_PROFILE
-aws configure set source_profile default --profile $AWS_CLI_PROFILE
+ROOT=$(git rev-parse --show-toplevel)
+cd $ROOT
 
-function get_es_secret () {
-  aws secretsmanager get-secret-value \
-    --secret-id "$1" \
-    --profile "$AWS_CLI_PROFILE" \
-    --output text \
-    --query 'SecretString'
-}
+export AWS_PROFILE=catalogue-developer
+export APP_CONTEXT=remote
+export secrets_resolver=AWSDefault
+export es_host="elasticsearch/concepts-$CONCEPTS_PIPELINE_DATE/public_host"
+export es_password="elasticsearch/concepts-$CONCEPTS_PIPELINE_DATE/ingestor/password"
 
-ES_HOST=$(get_es_secret "elasticsearch/concepts-$CONCEPTS_PIPELINE_DATE/public_host")
-ES_PORT=9243
-ES_SCHEME="https"
-ES_USERNAME="ingestor"
-ES_PASSWORD=$(get_es_secret "elasticsearch/concepts-$CONCEPTS_PIPELINE_DATE/ingestor/password")
+sbt "project ingestor" run "$@"
 
-(cd $ROOT && docker compose run \
-  --env es_host="$ES_HOST" \
-  --env es_port="$ES_PORT" \
-  --env es_scheme="$ES_SCHEME" \
-  --env es_username="$ES_USERNAME" \
-  --env es_password="$ES_PASSWORD" \
-  ingestor "$@")
+
+
--- ingestor/src/main/scala/weco/concepts/ingestor/Main.scala
+++ ingestor/src/main/scala/weco/concepts/ingestor/Main.scala
@@ -3,20 +3,26 @@ package weco.concepts.ingestor
 import grizzled.slf4j.Logging
 import org.apache.pekko.Done
 
+import scala.concurrent.duration.Duration
+import scala.concurrent.{Await, Future}
 import scala.util.{Failure, Success}
 
 object Main extends App with IngestorMain with Logging {
-  ingestStream.run
+  val future: Future[Done] = ingestStream.run
     .recover { case err: Throwable =>
-      error(err.getMessage); Done
+      error(err.getMessage)
+      Done
     }
-    .onComplete { result =>
-      result match {
-        case Success(_) =>
-          info("Execution completed successfully")
-        case Failure(exception) =>
-          error(s"Execution failed with $exception")
-      }
-      actorSystem.terminate()
+
+  future.onComplete { result =>
+    result match {
+      case Success(_) =>
+        info("Execution completed successfully")
+      case Failure(exception) =>
+        error(s"Execution failed with $exception")
     }
+    actorSystem.terminate()
+  }
+
+  Await.result(actorSystem.whenTerminated, Duration.Inf)
 }
