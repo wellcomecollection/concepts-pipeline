5f1c28634b9c22359a8583247aa126af862e1140
BuildKite on behalf of Wellcome Collection <wellcomedigitalplatform@wellcome.ac.uk>
2024-11-15 08:57:11 +0000

    Point concepts-pipeline at 2024-11-05

--- infrastructure/2022-08-31/main.tf
+++ infrastructure/2022-08-31/main.tf
@@ -5,7 +5,7 @@ module "concepts_pipeline" {
 
   # This is the namespace of the works catalogue pipeline
   # that this concept pipeline should subscribe to for changes.
-  catalogue_namespace = "2024-08-15" // This is automatically bumped by the catalogue-api repo
+  catalogue_namespace = "2024-11-05" // This is automatically bumped by the catalogue-api repo
 
   network_config     = local.network_config
   logging_cluster_id = local.logging_cluster_id
